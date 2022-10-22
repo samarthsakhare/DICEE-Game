2decfe09c307a1d41433e2aa18cf82e1c954db2b
samarthsakhare <102915172+samarthsakhare@users.noreply.github.com>
2022-10-22 16:17:10 +0530

    Add files via upload

---
+++ ext.js
@@ -0,0 +1,24 @@
+let randomNumber1 = Math.floor(Math.random()*6) + 1;
+console.log(randomNumber1);
+
+let randomNumber2 = Math.floor(Math.random()*6) + 1;
+console.log(randomNumber2);
+
+let randomNumber1s = "images/dice" + randomNumber1 + ".png";
+let randomNumber2s = "images/dice" + randomNumber2 + ".png";
+
+let a = document.querySelectorAll("img")[0];
+a.setAttribute("src",randomNumber1s);
+
+let b = document.querySelectorAll("img")[1];
+b.setAttribute("src",randomNumber2s);
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("#h").textContent = "Player 1 Win";
+}
+else if(randomNumber1<randomNumber2){
+    document.querySelector("#h").textContent = "Player 2 Win";
+}
+else{
+    document.querySelector("#h").textContent = "Tie";
+}
